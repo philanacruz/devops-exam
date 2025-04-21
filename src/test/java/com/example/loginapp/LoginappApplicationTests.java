a6a7f6f7ab55d6319faee69e431635a2a4fdb291
philanacruz <102311559+philanacruz@users.noreply.github.com>
2025-04-21 19:04:07 +0530

    Update LoginappApplicationTests.java

--- src/test/java/com/example/loginapp/LoginappApplicationTests.java
+++ src/test/java/com/example/loginapp/LoginappApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.loginapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LoginappApplicationTests {
+//@SpringBootTest
+//class LoginappApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
-}
+//}
